2d4fc96a03abc0f9fe3164e27ad7d76380e3d0aa
Jason Blocklove <jason.blocklove@gmail.com>
2020-03-03 16:47:57 -0500

    Added dmenu scripts

--- stow_files/dmenu/.config/dmenu/scripts/dmenu_ssh.sh
+++ stow_files/dmenu/.config/dmenu/scripts/dmenu_ssh.sh
@@ -2,17 +2,25 @@
 
 SSH_CMD="st -e ssh "
 
+COLOR_DMENU="$HOME/.config/dmenu/scripts/color_dmenu.sh "
+
 function get_hosts(){
-	while read line; do
+	readarray -t ssh_configs < $HOME/.ssh/config
+	for (( i = 0; i < ${#ssh_configs[@]}; i++ )); do
+		line=${ssh_configs[$i]}
 		label=$(echo $line | cut -d' ' -f1)
 		if [[ $label == "Host" ]]; then
-			host=$(echo $line | cut -d' ' -f2)
-			echo $host
+			if [[ $(echo ${ssh_configs[$i+1]} |  cut -d' ' -f1) == "HostName" ]]; then
+				host=$(echo $line | cut -d' ' -f2)
+				echo $host
+			fi
 		fi
-	done < $HOME/.ssh/config
+	done
 
 }
 
-SEL=$( get_hosts | dmenu -c -l 10 -p "ssh:" | awk '{print $1}' )
+SEL=$( get_hosts | $COLOR_DMENU -c -l 10 -p "ssh:" | awk '{print $1}' )
 
-$SSH_CMD $SEL
+if [[ ! -z $SEL ]]; then
+	$SSH_CMD $SEL
+fi
