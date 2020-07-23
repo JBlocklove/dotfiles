0229a35bb267cb42efe35c1475255eebd8e93690
Jason Blocklove <jason.blocklove@gmail.com>
2020-07-23 14:42:25 -0400

    Updates to dwm

--- stow_files/dwm/.config/dwm/autostart.sh
+++ stow_files/dwm/.config/dwm/autostart.sh
@@ -1,16 +1,18 @@
-## Run only at login
-compton &
-#wal -R &
-
-$XDG_CONFIG_HOME/wm/scripts/set_last_wp.sh &
-dwm_statusbar &
-
-sleep 1
-
-blueman-applet &
-
-cd $HOME
-franz &
-sleep 2
-signal-desktop-beta &
-#$HOME/.local/bin/neomutt_run
+### Run only at login
+#compton &
+##wal -R &
+#
+#wm_set_last_wp &
+#dwm_statusbar &
+#
+#mopidy &
+#
+#sleep 1
+#
+#blueman-applet &
+#
+#cd $HOME
+#ferdi &
+#sleep 2
+#signal-desktop-beta &
+#neomutt_run
