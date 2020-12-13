5bb77d1944d30ae95d130ee4558c9fffe9a4b9b6
Jason Blocklove <jason.blocklove@gmail.com>
2020-12-13 08:50:17 -0500

    Even more small fixes

--- program_installs/dwm/dwm.sh
+++ program_installs/dwm/dwm.sh
@@ -8,11 +8,14 @@
 pushd .
 cd /tmp
 git clone https://github.com/JBlocklove/dwm.git
+git clone https://github.com/JBlocklove/dmenu.git
 cd dwm
+sudo make install
+cd ../dmenu
 sudo make install
 popd
 rm -rf /tmp/st
 
 cd $DOTFILES_INSTALL/stow_files
-stow --verbose -t ~/ dwm wm pywal
+stow --verbose -t ~/ dwm wm pywal dmenu
 cd -
