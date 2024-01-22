b850437f20718d0d5a34a66e594bca1647e58559
Jason Blocklove <jason.blocklove@gmail.com>
2024-01-22 00:40:31 -0500

    Removed a bunch of stuff

--- program_installs/calcurse/calcurse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-########################################
-# Base install of calcurse
-########################################
-
-cd $DOTFILES_INSTALL/stow_files
-stow --verbose -t ~/ calcurse
-cd -
--- program_installs/nvim/nvim.sh
+++ program_installs/nvim/nvim.sh
@@ -9,10 +9,3 @@
 cd $DOTFILES_INSTALL/stow_files
 stow --verbose -t ~/ nvim
 cd -
-
-echo "Installing plugins"
-# Install/update vim-plug
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-# Install plugins
-nvim +PackerSync
